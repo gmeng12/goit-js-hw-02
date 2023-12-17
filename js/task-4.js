758f41a6207ce2a6012f29b0da82d5a34bd36f91
gmeng12 <6080043@gmail.com>
2023-12-17 14:45:17 +0100

    hw-js-02

--- js/task-4.js
+++ js/task-4.js
@@ -1,30 +1,25 @@
 'use strict'
 function getShippingCost(country) {
     let price;
-    let message;
 
     switch (country) {
         case 'China':
             price = 100;
-            message = `Shipping to ${country} will cost ${price} credits`;
             break;
         case 'Chile':
             price = 250;
-            message = `Shipping to ${country} will cost ${price} credits`;
             break;
         case 'Australia':
             price = 170;
-            message = `Shipping to ${country} will cost ${price} credits`;
             break;
         case 'Jamaica':
             price = 120;
-            message = `Shipping to ${country} will cost ${price} credits`;
             break;
         default:
-            message = 'Sorry, there is no delivery to your country'
+            return 'Sorry, there is no delivery to your country'
     }
+    const message = `Shipping to ${country} will cost ${price} credits`;
     return message;
-
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
